26a51603625a4c23cedbee414e39d03fea3eec79
Jan Rohwer <jan.rohwer@gmail.com>
2023-01-15 21:24:39 +0100

    init-file completed

--- .zsh/init.sh
+++ .zsh/init.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 echo "initializing dotfiles..."
+DOTFILES_DIR=$HOME/.dotfiles
+
+git clone --bare https://github.com/janhuddel/dotfiles.git $DOTFILES_DIR
+git --git-dir=$DOTFILES_DIR --work-tree=$HOME' checkout
