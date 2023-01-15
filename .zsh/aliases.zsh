19e651d1f069d94dd9ed090e905867808649e280
Jan Rohwer <jan.rohwer@gmail.com>
2023-01-15 16:44:01 +0100

    new aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,5 +1,14 @@
 alias dotfiles='$GIT_EXE --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
+alias ..="cd .."
 alias ls="exa --icons --group-directories-first"
 alias ll="exa --icons --group-directories-first -l"
 alias la="exa --icons --group-directories-first -la"
+
+# docker
+alias dc="docker compose"
+alias dps="docker ps"
+alias dpi="docker image"
+
+# git
+alias gst="git status"
