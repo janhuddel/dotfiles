eb27f2a102425c603691195a73f2149a547d9ab1
Jan Rohwer <jan.rohwer@gmail.com>
2023-01-15 16:13:33 +0100

    new version (with starship)

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,5 @@
+alias dotfiles='$GIT_EXE --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias la="exa --icons --group-directories-first -la"
---
+++ .zshrc
@@ -0,0 +1,7 @@
+GIT_EXE=$(which git)
+
+[[ -f ~/.zsh/aliases.zsh ]] && source ~/.zsh/aliases.zsh
+[[ -f ~/.zsh/starship.zsh ]] && source ~/.zsh/starship.zsh
+
+# Load Starship
+eval "$(starship init zsh)"
