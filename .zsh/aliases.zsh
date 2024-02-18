0ecae55e56e71f4fd689fdc778b2a8024eecd2e9
Jan Rohwer <jan.rohwer@gmail.com>
2024-02-18 13:58:15 +0100

    update

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,6 +4,12 @@ if (( ${+commands[eza]} )); then
 	alias ls="eza --group-directories-first"
 	alias ll="eza --group-directories-first -l"
 	alias la="eza --group-directories-first -la"
+else
+	alias l="ls -lah"
+	alias la="ls -lAh"
+	alias ll="ls -lh"
+	alias ls="ls --color=tty"
+	alias lsa="ls -lah"
 fi
 
 alias ..="cd .."
