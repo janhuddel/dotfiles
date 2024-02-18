b584fecb03c23b48548a166c74d7252837d80821
Jan Rohwer <jan.rohwer@gmail.com>
2024-02-18 13:56:26 +0100

    update

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,9 +1,14 @@
+# eza (ls replacement)
+# https://github.com/eza-community/eza/
+if (( ${+commands[eza]} )); then
+	alias ls="eza --group-directories-first"
+	alias ll="eza --group-directories-first -l"
+	alias la="eza --group-directories-first -la"
+fi
+
 alias ..="cd .."
 alias home="cd ~ && clear"
 alias cls="clear"
-alias ls="eza --group-directories-first"
-alias ll="eza --group-directories-first -l"
-alias la="eza --group-directories-first -la"
 
 # git
 alias gst="git status"
