de8ca79174d2c095e8c3bf87856ec61bdb83be36
Jan Rohwer <jan.rohwer@gmail.com>
2024-02-19 10:21:01 +0100

    nvm on linux

--- .zsh/nvm.zsh
+++ .zsh/nvm.zsh
@@ -2,4 +2,11 @@
 if brew --prefix nvm &> /dev/null ; then
 	export NVM_DIR=~/.nvm
 	source $(brew --prefix nvm)/nvm.sh
+
+# nvm on linux
+elif [ -f $HOME/.nvm/nvm.sh ]; then
+	export NVM_DIR=~/.nvm
+	source $NVM_DIR/nvm.sh
+	source $NVM_DIR/bash_completion
+
 fi
